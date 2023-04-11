6008ceb848f8016c3cf7fb52f9a94a161db84702
hhwaaaaa1 <hhwaaaaa1@naver.com>
2023-04-11 15:23:31 +0900

    Add clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,38 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
 const ejs = require('gulp-ejs');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
 
-function html() {
+gulp.task('html', function () {
   return gulp
     .src('src/**/*.ejs')
     .pipe(ejs())
     .pipe(rename({ extname: '.html' }))
     .pipe(gulp.dest('./dist'));
-}
+});
 
-function css() {
-  return gulp.src('src/**/*.scss').pipe(sass()).pipe(gulp.dest('./dist'));
-}
+gulp.task('css', function () {
+  return gulp
+    .src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist'));
+});
 
-const build = gulp.series(html, css);
+gulp.task('clean', function () {
+  return gulp.src('dist/*').pipe(clean());
+});
 
-function serve() {
-  browserSync.init({ server: { baseDir: './dist' } });
-  gulp.watch('./src/**/*', gulp.series(build, browserSync.reload));
-}
+gulp.task('build', gulp.series('clean', 'html', 'css'));
 
-exports.default = build;
-exports.build = build;
-exports.serve = serve;
+gulp.task('serve', function () {
+  browserSync.init({
+    server: { baseDir: './dist' },
+  });
+
+  gulp.watch(
+    './src/**/*',
+    gulp.series('build', browserSync.reload)
+  );
+});
